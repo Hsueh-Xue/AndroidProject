4dec6c2b7c3c6fe1c1c7162816f9d61c77b9e0ca
xueht0525 <867997466@qq.com>
2019-12-06 22:43:43 +0800

    二维码完成

--- MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        final ImageView imageView = (ImageView) findViewById(R.id.image);
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String URL = editText.getText().toString();
+                Bitmap bitmap = QRCodeUtil.createQRCodeBitmap(URL, 800, 800, "UTF-8", "H", "1",
+                        Color.GREEN, Color.WHITE);
+                imageView.setImageBitmap(bitmap);
+            }
+        });
     }
 }

990e6b75394e7c57cf6dcb4d66f319d39f0cda8c
xueht0525 <867997466@qq.com>
2019-12-06 17:58:47 +0800

    init

---
+++ FinalProject/app/src/main/java/com/example/finalproject/ui/map/MapViewModel.java
@@ -0,0 +1,7 @@
+package com.example.finalproject.ui.map;
+
+import androidx.lifecycle.ViewModel;
+
+public class MapViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ FinalProject/app/src/main/java/com/example/finalproject/ui/weather/WeatherViewModel.java
@@ -0,0 +1,7 @@
+package com.example.finalproject.ui.weather;
+
+import androidx.lifecycle.ViewModel;
+
+public class WeatherViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}

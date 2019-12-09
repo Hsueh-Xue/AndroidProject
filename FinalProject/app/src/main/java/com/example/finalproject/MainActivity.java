fbddbebaadec7d92328b526728911b740e69e3b1
xueht0525 <867997466@qq.com>
2019-12-09 08:30:13 +0800

    modify some warnings

--- FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

8c2d00471b3acdfe997186b5fe7f8ee7cbaf6724
xueht0525 <867997466@qq.com>
2019-12-09 18:49:43 +0800

    二维码生成

--- FinalProject/app/src/main/java/com/example/finalproject/Weather/RecommendFragment.java
+++ FinalProject/app/src/main/java/com/example/finalproject/Weather/RecommendFragment.java
@@ -1,7 +1,10 @@
 package com.example.finalproject.Weather;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,8 +15,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.finalproject.R;
+import com.example.finalproject.util.QRCodeUtil;
+import com.google.zxing.qrcode.encoder.QRCode;
 
 public class RecommendFragment extends Fragment {
     private static final String TAG = "RecommendFragment";
@@ -33,5 +41,19 @@ public class RecommendFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        final EditText editText = (EditText) view.findViewById(R.id.editText);
+        Button button = (Button) view.findViewById(R.id.button);
+        final ImageView imageView = (ImageView) view.findViewById(R.id.image);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String URL = editText.getText().toString();
+                Log.i(TAG, URL);
+                Bitmap bitmap = QRCodeUtil.createQRCodeBitmap(URL, 200, 200, "UTF-8", "H", "1",
+                        Color.BLACK, Color.WHITE);
+                imageView.setImageBitmap(bitmap);
+            }
+        });
     }
 }

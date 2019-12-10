3329592896973d0d9a0acf9a7610383170ea0d50
xueht0525 <867997466@qq.com>
2019-12-10 18:13:58 +0800

    up

--- FinalProject/app/src/main/java/com/example/finalproject/SettingsFragment.java
+++ FinalProject/app/src/main/java/com/example/finalproject/SettingsFragment.java
@@ -11,10 +11,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.finalproject.util.UserManage;
 
 
 public class SettingsFragment extends Fragment {
+
+    private static final String TAG = "SettingsFragment";
+    private Button nav_button;
+    private TextView welcome;
+    private EditText oldPassword;
+    private EditText newPassword;
+    private EditText prePassword;
+    private Button changePasswordButton;
+    private Button dayNumberButton;
 
     public SettingsFragment() {
 
@@ -30,5 +43,21 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        nav_button = view.findViewById(R.id.nav_button);
+        welcome = view.findViewById(R.id.welcome);
+        oldPassword = view.findViewById(R.id.old_password);
+        newPassword = view.findViewById(R.id.new_password);
+        prePassword = view.findViewById(R.id.pre_password);
+        changePasswordButton = view.findViewById(R.id.change_password_button);
+        dayNumberButton = view.findViewById(R.id.day_number);
+
+
+        changePasswordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userName = UserManage.getInstance().getUserName(getActivity());
+
+            }
+        });
     }
 }
